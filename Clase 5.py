b35b8d020c33537d166de60fbd456ad1ba33d297
DiegoCarazo <126536443+DiegoCarazo@users.noreply.github.com>
2023-06-08 15:10:43 -0600

    Update Clase 5.py

--- Clase 5.py
+++ Clase 5.py
@@ -10,9 +10,16 @@ def calcular_salario_total(salarios):
     salario_total = calcular_salario_total(salarios)
 
     print("El total pagado por la empresa por concepto de salarios es:", salario_total)
-
-
-
+    
+# Desarrolle un programa que calcule y muestre la suma de los números pares contenidos en 10 valores enteros ingresados por el usuario.
+def Suma_de_num():
+    num = 0
+    num_input = float(input('Ingrese los 10 numeros a calcular: '))
+    for i in num_input.range(10):
+        if i% == 0:
+            print(i)
+            i += 1
+    
 
 
 print('Bienvenido a las clase 5, para está oportunidad tenemos 3 programas')
