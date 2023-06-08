673c718f3ec6b3bec762435dca93216a029f9d4b
DiegoCarazo <126536443+DiegoCarazo@users.noreply.github.com>
2023-06-08 14:59:45 -0600

    Create Clase 1.py

---
+++ Clase 1.py
@@ -0,0 +1,61 @@
+#Ejercicio 1
+#variables = []
+#for i in range(4):
+#    variable = int(input('Ingrese la variable {}: '.format(i + 1)))
+#    variables.append(variable)
+#    rever = variables[::-1]
+#print(rever)
+
+
+# Ejercicio 2
+#edad = int(input('¿Cuál es su edad? \n'))
+#print('Dentro de 5 años tendra: ', edad + 5)
+
+#Ejercicio 3
+#numero_1 = int(input('¿Cuál es el valor de "A"?\n'))
+#numero_2 = int(input('¿Cuál es el valor de "B"?\n'))
+#operacion = (((numero_1 + numero_2)/2)*3 )
+#print('El resultado de la operacion es: ', operacion)
+
+#Ejercicio 4
+#Numero_solicitado = float(input('Ingrese el numero para el calculo: '))
+#cuadrado = Numero_solicitado **2
+#cubo = Numero_solicitado **3
+#print('Su numero al cuadrado es: ', cuadrado, '\nSu numero al cubo es: ', cubo)
+
+#Ejercicio 5
+#base = float(input('¿Cuál es la base del rectangulo: '))
+#altura = float(input('¿Cuál es la altura del rectangulo: '))
+#print('El area del rectangulo es: ', base * altura, '\nLa el perimetro del rectangulo es: ',(altura * 2) + (base * 2))
+
+#Ejercicio 6
+#distancia = float(input('¿Cuál es la distancia a recorrer?'))
+#costo = float(input('¿Cuál es el costo hasta por kilometro?'))
+#dias = int(input('¿Cuantos dias a la semana debe ir a la universidad?'))
+#Traslado_diario = (distancia * costo) *2
+#print('EL costo del traslado diario es: ', Traslado_diario)
+#print('El costo del traslado semanal es  de: ', Traslado_diario * dias)
+#print('EL costo cruatrimestral es de: ', (Traslado_diario * dias) * 15)
+
+#Ejercicio 7
+#edades = []
+#for i in range(5):
+#    edad = int(input("Ingrese la edad de la persona {}: ".format(i + 1 )))
+#    edades.append(edad)
+#promedio = sum(edades) / len(edades)
+#print("El promedio de las edades ingresadas es:", promedio)
+
+#Ejercicio 8
+#horas_semanales = int(input('¿Cuántas horas por semana trabaja? '))
+#precio_hora = int(input('¿Cuanto gana por hora? '))
+#bruto = (horas_semanales * precio_hora) * 4.2
+#neto = bruto - ((bruto * (10.50/100)) + (bruto *(5/100)))
+#print('Su salario bruto es: ', bruto)
+#print('Su salario neto es: ', neto)
+
+#Ejercicio 9
+#ingreso_mensual = int(input('¿Cuál es su ingreso mensual? '))
+#gastos_alimentacion = int(input('¿Cuál es su gasto en alimentacion mensual? '))
+#porcentaje_alimentacion = (gastos_alimentacion / ingreso_mensual) * 100
+#print('Su salario remanente es de: ', ingreso_mensual - gastos_alimentacion)
+#print('En porcentaje es: ', round(porcentaje_alimentacion),'%')
